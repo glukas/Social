f9af89355298d10907bda3092fbbf6b5d3484e76
Lukas Gianinazzi <orionweissnix@gmail.com>
2014-11-12 17:45:07 +0100

    Initial Commit

---
+++ Social/gen/ch/ethz/inf/vs/android/glukas/project4/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ch.ethz.inf.vs.android.glukas.project4;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
