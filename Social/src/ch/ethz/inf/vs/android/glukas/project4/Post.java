c680780c2723aaac85ce11d6e9655df11dc3a76e
Young Ban <bany@student.ethz.ch>
2014-11-20 00:39:31 +0100

    Post.java didn't commit last time for some reason
    - Shouldve been commited yesterday but didnt for some reason..

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/Post.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/Post.java
@@ -5,11 +5,31 @@ package ch.ethz.inf.vs.android.glukas.project4;
  */
 
 public class Post {
-	
+
 	// author: Alessio
 	// just to begin
+
+	// Text
 	public String text;
+
+	// Image Binary
 	public String image;
+
+	// Id of the Post
+	public int id;
+
+	// Lamport timestamp instead of real time
+	// they are unique for each user
+	public int timestamp;
+
+	public Post(String text, String image, int id, int timestamp) {
+		this.text = text;
+		this.image = image;
+		this.id = id;
+		this.timestamp = timestamp;
+
+	}
+
 	// metadata: date/time, visited times, ...
-	
+
 }
