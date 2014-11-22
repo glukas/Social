9a783313bafc9563a9eccf04c393202820ded56f
Lukas Gianinazzi <orionweissnix@gmail.com>
2014-11-22 16:55:45 +0100

    (+) Symmetric key generation

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/security/KeyGeneration.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/security/KeyGeneration.java
@@ -1,10 +1,15 @@
 package ch.ethz.inf.vs.android.glukas.project4.security;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class KeyGeneration {
 
 	static final KeyGeneration keyGen = new KeyGeneration();
+	
 	
 	public static final KeyGeneration getInstance() {
 		return keyGen;
@@ -13,10 +18,34 @@ public class KeyGeneration {
 	//Note: not so "secure" on android 4.3 and lower.
 	private SecureRandom secureRandom = new SecureRandom();
 	
+	private KeyGenerator encryptionKeyGenerator;
+	
+	private KeyGenerator macKeyGenerator;
+	
+	private KeyGeneration() {
+		try {
+			encryptionKeyGenerator = KeyGenerator.getInstance("AES");
+			macKeyGenerator = KeyGenerator.getInstance("HmacSHA256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public byte[] getPseudorandom(int numberOfBytes) {
 		byte[] bytes = new byte[numberOfBytes];
 		secureRandom.nextBytes(bytes);
 		return bytes;
 	}
 	
+	public byte[] generateUserId() {
+		return getPseudorandom(16);
+	}
+	
+	public SecretKey generateEncryptionKey() {
+		return encryptionKeyGenerator.generateKey();
+	}
+	
+	public SecretKey generateMACKey() {
+		return macKeyGenerator.generateKey();
+	}
 }
