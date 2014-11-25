0cd1a0c2d2914b05c35edef934ae239311d3a585
Tom_Tenv <vdemotz@student.ethz.ch>
2014-11-25 13:12:45 +0100

    (/=) PublicHeader
    (/=) Fix header getBytes() and creation from byte array

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/UserId.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/UserId.java
@@ -8,6 +8,7 @@ import java.math.BigInteger;
 public class UserId {
 
 	private BigInteger id;
+	public static final int LENGTH = 16;
 
 	public UserId(String value) {
 		id = new BigInteger(value);
@@ -23,5 +24,25 @@ public class UserId {
 	
 	public String toString() {
 		return "userId : " + id;
+	}
+	
+	/**
+	 * Get a byte array representing this UserId
+	 */
+	public byte[] getBytes() {
+		byte[] array = id.toByteArray();
+		if (array.length < LENGTH){
+			byte extendByte = (array[0] < 0 ? (byte)0xff : new Byte("0"));
+			byte[] tmp = new byte[LENGTH];
+			for (int i = 0; i < LENGTH; i++){
+				if (array.length + i >= LENGTH){
+					tmp[i] = array[i - (LENGTH - array.length)];
+				} else {
+					tmp[i] = extendByte;
+				}
+			}
+			array = tmp;
+		} 
+		return array;
 	}
 }
