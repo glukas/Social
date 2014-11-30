864ddb4a625d00f26b98d0dac2593b1aec9d8fda
Sam <->
2014-11-30 12:57:56 +0100

    stuff

--- Social/gen/ch/ethz/inf/vs/android/glukas/project4/R.java
+++ Social/gen/ch/ethz/inf/vs/android/glukas/project4/R.java
@@ -25,14 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f080008;
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000a;
         public static final int connectButton=0x7f080003;
         public static final int connection_screen_info=0x7f080002;
         public static final int contactTextView=0x7f080000;
         public static final int home_add_friend_button=0x7f080006;
-        public static final int home_friends_button=0x7f080004;
+        public static final int home_screen=0x7f080004;
+        public static final int home_view_friends_button=0x7f080008;
         public static final int home_wall_button=0x7f080005;
+        public static final int tabtextview=0x7f080009;
         public static final int usernameTextView=0x7f080001;
         public static final int wall=0x7f080007;
     }
@@ -41,6 +42,7 @@ public final class R {
         public static final int activity_incoming_friendship=0x7f030001;
         public static final int connection_screen=0x7f030002;
         public static final int home_screen=0x7f030003;
+        public static final int view_friends=0x7f030004;
     }
     public static final class menu {
         public static final int contact_detail=0x7f070000;
--- Social/src/ch/ethz/inf/vs/android/glukas/project4/MainActivity.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/MainActivity.java
@@ -20,24 +20,31 @@ import ch.ethz.inf.vs.android.glukas.project4.test.StaticDatabase;
 import ch.ethz.inf.vs.android.glukas.project4.test.StaticSecureChannel;
 
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.OnNdefPushCompleteCallback;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnNdefPushCompleteCallback, UserDelegate, SecureChannelDelegate {
 
 	private static final String tag = "MAIN_ACTIVITY";
-	private Button mConnectButton, mAddFriendButton, mViewWallButton;
+	private Button mAddFriendButton, mViewWallButton, mViewFriendsButton;
 	private AlertDialog.Builder mAlertBuilder;
 
 	NfcAdapter nfcAdapter;
@@ -50,10 +57,30 @@ public class MainActivity extends Activity implements OnNdefPushCompleteCallback
 	StaticDatabase db;
 	StaticSecureChannel channel;
 
+	ActionBar.Tab tab1, tab2, tab3;
+	Fragment fragmentTab1 = new FragmentTab1();
+	Fragment fragmentTab2 = new FragmentTab2();
+	Fragment fragmentTab3 = new FragmentTab3();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.connection_screen);
+		setContentView(R.layout.home_screen);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		tab1 = actionBar.newTab().setText("1");
+		tab2 = actionBar.newTab().setText("2");
+		tab3 = actionBar.newTab().setText("3");
+
+		tab1.setTabListener(new MyTabListener(fragmentTab1));
+		tab2.setTabListener(new MyTabListener(fragmentTab2));
+		tab3.setTabListener(new MyTabListener(fragmentTab3));
+
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
+		actionBar.addTab(tab3);
 
 		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		if (nfcAdapter == null) {
@@ -61,28 +88,27 @@ public class MainActivity extends Activity implements OnNdefPushCompleteCallback
 			finish();
 			return;
 		}
-		
+
 		mAlertBuilder = new AlertDialog.Builder(this);
-		
+
 		mProtocol = Protocol.getInstance(new DatabaseManager(this));
 		mProtocol.setDelegate(this);
-		mConnectButton = (Button)findViewById(R.id.connectButton);
-
-		mConnectButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				mProtocol.connect();
-			}
-		});
 
 		mAddFriendButton = (Button)findViewById(R.id.home_add_friend_button);
-
 		mAddFriendButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				showFriendRequest();
+				// TODO: Implement adding friends by username.
+			}
+		});
+
+		mViewFriendsButton = (Button)findViewById(R.id.home_view_friends_button);
+
+		mViewFriendsButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				showFriendsList();
 			}
 		});
 
@@ -97,7 +123,7 @@ public class MainActivity extends Activity implements OnNdefPushCompleteCallback
 		});
 
 		createNextRequest();
-		
+
 		//TODO TESTING
 		/*
 		channel = new StaticSecureChannel("winti.mooo.com", 9000);
@@ -106,36 +132,16 @@ public class MainActivity extends Activity implements OnNdefPushCompleteCallback
 		PublicHeader header = new PublicHeader(44, null, StatusByte.CONNECT.getByte(), 0, Data.dummySenderId, Data.serverId);
 		channel.sendHeader(header);
 		Log.i("DEBUG", Data.tag+"header send");
-		*/
+		 */
 	}
 
 
 	private void showWall() {
-		
+
 	}
 
-	private void showFriendRequest() {
-		mAlertBuilder.setTitle("Title");
-		mAlertBuilder.setMessage("Message");
+	private void showFriendsList() {
 
-		// Set an EditText view to get user input 
-		final EditText input = new EditText(this);
-		mAlertBuilder.setView(input);
-
-		mAlertBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				String userName = input.getText().toString();
-				mProtocol.askFriendship(userName);
-			}
-		});
-
-		mAlertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				dialog.cancel();
-			}
-		});
-
-		mAlertBuilder.show();
 	}
 
 	private void createNextRequest() {
@@ -205,6 +211,56 @@ public class MainActivity extends Activity implements OnNdefPushCompleteCallback
 	public void onMessageReceived(NetworkMessage message) {
 		Log.d("DEBUG", Data.tag+"Message received : "+MessageParser.parseMessage(message.text, message.header, db).toString());
 
+	}
+
+	public class FragmentTab1 extends Fragment {
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+				Bundle savedInstanceState){
+			View view = inflater.inflate(R.layout.view_friends, container, false);
+			TextView textview = (TextView) view.findViewById(R.id.tabtextview);
+			textview.setText("TEst");
+			return view;
+		}
+	}
+	
+	public class FragmentTab2 extends Fragment {
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+				Bundle savedInstanceState){
+			View view = inflater.inflate(R.layout.view_friends, container, false);
+			TextView textview = (TextView) view.findViewById(R.id.tabtextview);
+			textview.setText("TEst");
+			return view;
+		}
+	}
+	
+	public class FragmentTab3 extends Fragment {
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+				Bundle savedInstanceState){
+			View view = inflater.inflate(R.layout.view_friends, container, false);
+			TextView textview = (TextView) view.findViewById(R.id.tabtextview);
+			textview.setText("TEst");
+			return view;
+		}
+	}
+	
+	public class MyTabListener implements ActionBar.TabListener {
+		Fragment fragment;
+		
+		public MyTabListener(Fragment fragment) {
+			this.fragment = fragment;
+		}
+		
+	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			ft.replace(R.id.home_screen, fragment);
+		}
+		
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			ft.remove(fragment);
+		}
+		
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// nothing done here
+		}
 	}
 
 }
