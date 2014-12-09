fe70a00a473c0943801d075309a1f717514e41d0
Lukas Gianinazzi <orionweissnix@gmail.com>
2014-12-09 17:47:01 +0100

    (/=) Different solution to test context
    -I may be totally wrong about this but it seems to work ok
    -this.mContext might also be an option

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/test/DatabaseTest.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/test/DatabaseTest.java
@@ -1,5 +1,6 @@
 package ch.ethz.inf.vs.android.glukas.project4.test;
 
+import java.lang.reflect.Method;
 import java.math.BigInteger;
 
 import org.junit.Test;
@@ -7,14 +8,17 @@ import org.junit.Test;
 import ch.ethz.inf.vs.android.glukas.project4.User;
 import ch.ethz.inf.vs.android.glukas.project4.database.DatabaseAccess;
 import ch.ethz.inf.vs.android.glukas.project4.database.DatabaseManager;
+import android.content.Context;
 import android.test.AndroidTestCase;
 import android.test.InstrumentationTestCase;
+import android.test.IsolatedContext;
+import android.test.ServiceTestCase;
 
-public class DatabaseTest extends InstrumentationTestCase{
+public class DatabaseTest extends AndroidTestCase{
 	
 	@Test
 	public void testUserInsertion() {
-		DatabaseAccess db = new DatabaseManager(getInstrumentation().getContext());
+		DatabaseAccess db = new DatabaseManager(getTestContext());
 		String username = "MeIAndMySelf";
 		User user = new User(username);
 		db.putUser(user);
@@ -38,5 +42,22 @@ public class DatabaseTest extends InstrumentationTestCase{
 //		boolean compare = id1.toString().equals(id2.toString());
 //		assertTrue(compare);
 //	}
+	
+	/**
+	 * @return The {@link Context} of the test project.
+	 */
+	private Context getTestContext()
+	{
+	    try
+	    {
+	        Method getTestContext = AndroidTestCase.class.getMethod("getTestContext");
+	        return (Context) getTestContext.invoke(this);
+	    }
+	    catch (final Exception exception)
+	    {
+	        exception.printStackTrace();
+	        return null;
+	    }
+	}
 
 }
