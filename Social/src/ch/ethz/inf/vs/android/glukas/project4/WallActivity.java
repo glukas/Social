9dd240b94fe2395565b722fe9e8d09e34e7494d5
Samuel Schmid <schmisam@student.ethz.ch>
2014-12-14 11:04:17 +0100

    Added softinputmode on ContactDetailActivity Added author textview on picture posts

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/WallActivity.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/WallActivity.java
@@ -1,10 +1,6 @@
 package ch.ethz.inf.vs.android.glukas.project4;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import android.app.Activity;
 import android.app.AlertDialog;
