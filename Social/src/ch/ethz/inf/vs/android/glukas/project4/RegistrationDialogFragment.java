e4ba7f8b4bf5cf2923c7e38f44041a52a29348f9
Samuel Schmid <schmisam@student.ethz.ch>
2014-12-14 12:23:18 +0100

    Fixed empty usernames being allowed. Fixed minor bug with registration window. Cleaned up registration window layout.

--- Social/src/ch/ethz/inf/vs/android/glukas/project4/RegistrationDialogFragment.java
+++ Social/src/ch/ethz/inf/vs/android/glukas/project4/RegistrationDialogFragment.java
@@ -1,18 +1,19 @@
 package ch.ethz.inf.vs.android.glukas.project4;
 
-import ch.ethz.inf.vs.android.glukas.project4.database.DatabaseAccess;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnDismissListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 //Fragment layout for the registration dialog
 public class RegistrationDialogFragment extends DialogFragment {
-
+	
+	private EditText input;
+	
 	public interface RegistrationDialogFragmentDelegate {
 		void onUserRegistered(String username);
 	}
@@ -21,9 +22,10 @@ public class RegistrationDialogFragment extends DialogFragment {
 
 	RegistrationDialogFragment(RegistrationDialogFragmentDelegate delegate) {
 		this.delegate = delegate;
-		
+		this.setCancelable(false);
 	}
 	
+	@SuppressLint("InflateParams") 
 	@Override
 	public AlertDialog onCreateDialog(Bundle savedInstanceState) {
 		// Use the Builder class for convenient dialog construction
@@ -32,32 +34,26 @@ public class RegistrationDialogFragment extends DialogFragment {
 		// Get the layout inflater
 		LayoutInflater inflater = getActivity().getLayoutInflater();
 
-		// Inflate and set the layout for the dialog
 		// Pass null as the parent view because its going in the dialog layout
-		//	        builder.setView(inflater.inflate(R.layout.dialog_registration, null));
-		final EditText input = new EditText(getActivity());
-		builder.setView(input);
+		final View dialogView = inflater.inflate(R.layout.dialog_registration, null);
+		
+		builder.setView(dialogView);
 
+		input = (EditText)dialogView.findViewById(R.id.dialog_username);
+		
 		// Set dialog title
 		builder.setTitle(R.string.registration_title);
 
 		// Set positive button
 		builder.setPositiveButton(R.string.register_button, new DialogInterface.OnClickListener() {
+
 			public void onClick(DialogInterface dialog, int id) {
 				// Retrieve data from textboxes, create new User and call putUser
-				String username = input.getEditableText().toString(); // TODO: check for null
-				Toast.makeText(getActivity(), username + " was succesfully registered", Toast.LENGTH_LONG).show();
-
+				String username = input.getEditableText().toString();
+				
 				delegate.onUserRegistered(username);
 			}
 		});
-		builder.setCancelable(false);
-		// Set negative button
-		/*builder.setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				// TODO: User cancelled registration dialog, what should be done?
-			}
-		});*/
 
 		// Create the AlertDialog object and return it
 		return builder.create();
